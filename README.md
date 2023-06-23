300b0e30a8c26e4398eec3cf4abf73500c870d71
Abdel @ StarkWare <abdelhamid.bakhta@gmail.com>
2023-06-23 17:59:58 +0200

    🎉 init dioxus project

---
+++ README.md
@@ -0,0 +1,17 @@
+# starkit
+
+## Publish
+
+### Desktop
+
+```bash
+cargo bundle --release
+```
+
+Once you've ran cargo-bundle --release, your app should be accessible in: `target/release/bundle/<platform>/`.
+
+### Web
+
+```bash
+trunk build --release
+```
---
+++ src/main.rs
@@ -0,0 +1,19 @@
+#![allow(non_snake_case)]
+// import the prelude to get access to the `rsx!` macro and the `Scope` and `Element` types
+use dioxus::prelude::*;
+
+fn main() {
+    // launch the dioxus app in a webview
+    dioxus_desktop::launch(App);
+}
+
+// define a component that renders a div with the text "Hello, world!"
+fn App(cx: Scope) -> Element {
+    let mut count = use_state(cx, || 0);
+
+    render!(
+        h1 { "High-Five counter: {count}" }
+        button { onclick: move |_| count += 1, "Up high!" }
+        button { onclick: move |_| count -= 1, "Down low!" }
+    )
+}
