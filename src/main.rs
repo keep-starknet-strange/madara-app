89de67e0c3b85f242f652aad09c578b6814d9c5f
Abdel @ StarkWare <abdelhamid.bakhta@gmail.com>
2023-06-26 14:00:53 +0200

    ✨ use rpc provider to query latest block number

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,11 @@
 extern crate lazy_static;
 // import the prelude to get access to the `rsx!` macro and the `Scope` and `Element` types
 use dioxus::prelude::*;
+use eyre::{eyre, Result};
 use starknet::providers::Provider;
 use starknet::{
-    core::types::{BlockId, BlockTag},
-    providers::{jsonrpc::HttpTransport, AnyProvider, JsonRpcClient, SequencerGatewayProvider},
+    core::types::{BlockId, BlockTag, MaybePendingBlockWithTxHashes},
+    providers::{jsonrpc::HttpTransport, JsonRpcClient},
 };
 use url::Url;
 
@@ -25,28 +26,27 @@ fn main() {
 
 // define a component that renders a div with the text "Hello, world!"
 fn App(cx: Scope) -> Element {
-    let mut count = use_state(cx, || 0);
-
+    let get_latest_block_future = use_future(cx, (), |_| async move { get_latest_block().await });
+    let latest_block = match get_latest_block_future.value() {
+        Some(latest_block) => *latest_block.as_ref().unwrap(),
+        None => 0_u64,
+    };
     render!(
-        h1 { "High-Five counter: {count}" }
-        button { onclick: move |_| {
-            /*let provider = get_starknet_rpc_provider();
-            let latest_block = provider
-            .get_block_with_tx_hashes(BlockId::Tag(BlockTag::Latest))
-            .await;
-        println!("{latest_block:#?}");*/
-            count += 1;
-        }, "Up high!" }
-        button { onclick: move |_| count -= 1, "Down low!" }
+        h1 { "Madara Kit" }
+        h2 { "Latest block: {latest_block}" }
     )
 }
 
-fn get_default_starknet_rpc_url() -> Url {
-    Url::parse("https://sharingan.madara.wtf").expect("Invalid Starknet RPC URL")
-}
-
-fn get_starknet_rpc_provider() -> JsonRpcClient<HttpTransport> {
-    starknet_rpc_provider(get_default_starknet_rpc_url())
+async fn get_latest_block() -> Result<u64> {
+    let latest_block = STARKNET_PROVIDER
+        .get_block_with_tx_hashes(BlockId::Tag(BlockTag::Latest))
+        .await?;
+    match latest_block {
+        MaybePendingBlockWithTxHashes::Block(block) => Ok(block.block_number),
+        MaybePendingBlockWithTxHashes::PendingBlock(_block) => {
+            Err(eyre!("pending block not supported"))
+        }
+    }
 }
 
 fn starknet_rpc_provider(rpc: Url) -> JsonRpcClient<HttpTransport> {
